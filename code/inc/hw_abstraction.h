1f0556e2a9aee90c9fab5158f3d07d86f4b856a4
vijay-hcl-epl <vijayone1708@gmail.com>
2026-02-24 10:28:17 +0530

    Add hw_abstraction.h header

--- code/inc/hw_abstraction.h
+++ code/inc/hw_abstraction.h
@@ -2,25 +2,16 @@
 #define HW_ABSTRACTION_H
 
 #include <stdint.h>
+#include <stdbool.h>
 
-// Platform-specific pin definitions and API prototypes
-#define MOTOR_DIR_PIN      (1)
-#define POWER_LED_PIN      (2)
-#define POS_LED_PIN_0      (3)
-#define POS_LED_PIN_1      (4)
-#define POS_LED_PIN_2      (5)
-#define POS_LED_PIN_3      (6)
-#define POS_LED_PIN_4      (7)
-#define POS_LED_PIN_5      (8)
-
-void hw_uart_init(void);
-uint8_t hw_uart_read(void);
-void hw_uart_write(uint8_t data);
-
-void hw_gpio_set(uint8_t pin, uint8_t value);
-
-uint16_t hw_adc_read(void);
-void hw_pwm_enable(void);
-void hw_pwm_disable(void);
+void hal_uart_init(void);
+bool hal_uart_rx_ready(void);
+uint8_t hal_uart_read(void);
+void hal_uart_write(uint8_t val);
+void hal_gpio_set(uint8_t pin);
+void hal_gpio_clear(uint8_t pin);
+uint16_t hal_adc_get_value(uint8_t channel);
+void hal_pwm_enable(void);
+void hal_pwm_disable(void);
 
 #endif // HW_ABSTRACTION_H
