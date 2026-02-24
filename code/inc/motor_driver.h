8a742b63d7343c6ae9ddb9284f31d45c5435f739
vijay-hcl-epl <vijayone1708@gmail.com>
2026-02-24 10:28:05 +0530

    Add motor_driver.h header

--- code/inc/motor_driver.h
+++ code/inc/motor_driver.h
@@ -1,16 +1,13 @@
 #ifndef MOTOR_DRIVER_H
 #define MOTOR_DRIVER_H
 
-#include <stdint.h>
-
 typedef enum {
-    MOTOR_DIRECTION_CW = 0,
-    MOTOR_DIRECTION_CCW
-} motor_direction_t;
+    DIR_CW = 0,
+    DIR_CCW
+} Direction;
 
-void motor_driver_init(void);
-void motor_driver_set_direction(motor_direction_t dir);
-void motor_driver_enable(void);
-void motor_driver_disable(void);
+void set_direction(Direction dir);
+void enable_motor(void);
+void disable_motor(void);
 
 #endif // MOTOR_DRIVER_H
