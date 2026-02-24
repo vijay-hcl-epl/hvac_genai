bbf7abc517df4f93a1143267c6e7c1290814dc9d
vijay-hcl-epl <vijayone1708@gmail.com>
2026-02-24 10:27:43 +0530

    Add position_sensing.h header

--- code/inc/position_sensing.h
+++ code/inc/position_sensing.h
@@ -1,17 +1,9 @@
 #ifndef POSITION_SENSING_H
 #define POSITION_SENSING_H
 
-#include <stdint.h>
 #include <stdbool.h>
 
-// Logical position definitions
-#define POSITION_INVALID 0xFF
-#define FLAP_POSITION_MIN 0
-#define FLAP_POSITION_MAX 5
-
-void position_sensing_init(void);
-uint8_t position_sensing_get_current_position(void);
-bool position_sensing_is_valid_position(uint8_t logical_pos);
-void position_sensing_periodic(void);
+bool get_current_position(int *pos);
+bool is_valid_position(void);
 
 #endif // POSITION_SENSING_H
