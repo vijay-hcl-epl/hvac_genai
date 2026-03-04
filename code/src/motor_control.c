06b666e9aad952ccb417ba49a025149d86ec8c47
vijay-hcl-epl <vijayone1708@gmail.com>
2026-03-04 21:45:43 +0530

    Add motor_control.c source (demo-43d branch) retry

---
+++ code/src/motor_control.c
@@ -0,0 +1,10 @@
+#include "motor_control.h"
+#include <stdbool.h>
+
+void MotorCtrl_Set(uint8_t direction, bool enable) {
+    // Set GPIO pins for direction and enable motor driver (pseudo-code)
+}
+
+void MotorCtrl_Stop(void) {
+    // Reset/disable GPIO pins to stop the motor driver (pseudo-code)
+}
