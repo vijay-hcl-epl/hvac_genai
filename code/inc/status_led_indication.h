83b5ff7fa090bafc23114372ea7729e71ffc88a7
vijay-hcl-epl <vijayone1708@gmail.com>
2026-03-04 21:30:38 +0530

    Add Status/LED Indication header

---
+++ code/inc/status_led_indication.h
@@ -0,0 +1,6 @@
+#ifndef STATUS_LED_INDICATION_H
+#define STATUS_LED_INDICATION_H
+#include <stdint.h>
+#include <stdbool.h>
+void LEDI_Update(uint8_t pos, bool error);
+#endif // STATUS_LED_INDICATION_H
